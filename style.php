3c330fcd761e4dfb280c4914c83311ded5c9fd61
wkwkrnht <wkwkrnht@gmail.com>
2017-01-08 22:12:03 +0900

    fix

--- style.php
+++ style.php
@@ -1580,4 +1580,13 @@ $toggle_color = get_option('toggle_color','#fff');?>
 			display:none;
 		}
 	}
+    .night-mode .hide-nav-prev a,.night-mode .hide-nav-next a,body.night-mode,.night-mode #main-menu,.night-mode .card,.night-mode div.card-list,.night-mode div#menu-wrap,.night-mode a#menu-toggle,.night-mode nav.menu-tab,.night-mode a#main-menu-toggle,.night-mode a#share-menu-toggle{color:#fff;background-color:#333;}
+	.night-mode ul.page-nation,.night-mode ul.page-nation a,.night-mode ul.page-nation li span.dots,.night-mode ul.page-nation li.current,.night-mode .page-nav,.night-mode .page-nav a:hover{color:#fff;background-color:#333;border-color:#fff;}
+	.night-mode ul.page-nation li span.dots{color:#f1f1f1;}
+	.night-mode ul.page-nation a:hover{color:#333;background-color:#fff;}
+	.night-mode .card,.night-mode ul.page-nation{box-shadow:0 0 3vmin rgba(0,0,0,.3);}
+	.night-mode div.article-meta,.night-mode .article-main h1,.night-mode div.information,.night-mode div.question{color:#333;}
+	.night-mode .article-main h2.ogp-blogcard-title,.night-mode .article-main p.ogp-blogcard-description,.night-mode .article-main a.ogp-blogcard-site-name,.night-mode .article-main h2.ogp-blogcard-title:visited,.night-mode .article-main p.ogp-blogcard-description:visited,.night-mode .article-main a.ogp-blogcard-site-name:visited,.night-mode .article-main img.ogp-blogcard-img::after{color:#fff;}
+	.night-mode div.ogp-blogcard{background-color:#333;border-color:#f1f1f1;}
+	.night-mode aside.toc{border:0;box-shadow:none;}
 </style>
