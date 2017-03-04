e54ee22442249df841c37240fa607b830c544c25
wkwkrnht <wkwkrnht@gmail.com>
2017-03-04 22:36:50 +0900

    fix

--- amp/content-list.php
+++ amp/content-list.php
@@ -86,7 +86,7 @@
     $big = 999999999;
     $page_format = paginate_links(array(
         'base'      => str_replace($big,'%#%',esc_url(get_pagenum_link($big))),
-        'format'    => '/page/%#%',
+        'format'    => '/page/%#%/amp',
         'current'   => max(1,get_query_var('paged')),
         'total'     => $wp_query->max_num_pages,
         'prev_next' => True,
