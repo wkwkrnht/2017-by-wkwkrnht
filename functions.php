9c6fc55eabf62418357219b63215c93ae1795405
wkwkrnht <wkwkrnht@gmail.com>
2017-04-22 21:25:28 +0900

    fix

--- functions.php
+++ functions.php
@@ -478,60 +478,7 @@ class google_translate_widget extends WP_Widget{
 class sns_follow extends WP_Widget{
     function __construct(){parent::__construct('sns_follow','バイラル風SNSフォローウィジェット',array());}
     public function widget($args,$instance){
-        echo $args['before_widget'] .
-        '<style>
-            .widget_sns_follow{}
-            .widget_sns_follow > .wrapper{
-                box-sizing:border-box;
-                display:inline-block;
-                margin:none;
-                width:49%;
-            }
-            .widget_sns_follow .description{
-                display:block;
-            }
-            .widget_sns_follow .wrapper:not(.eyecatch){
-                background-color:#333;
-                color:#fff;
-            }
-            .widget_sns_follow .follow-button-area{
-                display:flex;
-                jutify-conent:space-evenly;
-                list-style:none;
-            }
-            .widget_sns_follow .follow-button-area > .follow-button{
-                font-size:1.4rem;
-                margin:1em 0;
-                padding:.3em 0 .45em 0;
-                width:32%;
-            }
-            .widget_sns_follow .follow-button-area > .twitter{
-                background-color:#1b95e0;
-            }
-            .widget_sns_follow .follow-button-area > .feedly{
-                background-color:#2BB24C;
-            }
-            .widget_sns_follow .follow-button-area > .twitter:hover{
-                background-color:#31a3ea;
-            }
-            .widget_sns_follow .follow-button-area > .feedly:hover{
-                background-color:#2ebc50;
-            }
-            .widget_sns_follow .follow-button-area > .follow-button > a{
-                color:#fff;
-                text-align:center;
-            }
-        </style>
-        <img src="' . get_wkwkrnht_eyecatch(array(512,512)) . '" alt="eyrecatch" class="eyecatch wrapper">
-        <div class="wrapper">
-            <h3 class="title">' . wp_title('',false) . '</h3>
-            <p class="description">この記事が気に入ったらフォローお願いします！フォロー先で最新情報をお届けします。</p>
-            <ul class="follow-button-area">
-                <li class="twitter follow-button"><a href="https://twitter.com/' . get_twitter_acount() . '" title="Twitterでフォローする"><i class="fa fa-twitter" aria-hidden="true"></i>フォローする</a></li>
-                <li class="feedly follow-button"><a href="' . get_bloginfo('atom_url') . '" title="RSSで購読する"><i class="fa fa-rss" aria-hidden="true"></i>RSSで購読する</a></li>
-            </ul>
-        </div>'
-        . $args['after_widget'];
+        echo $args['before_widget'];include(get_template_directory() . '/widget/sns-follow.php');echo $args['after_widget'];
     }
 }
 
---
+++ widget/sns-follow.php
@@ -0,0 +1,54 @@
+<style>
+    .widget_sns_follow{
+        background-color:#333;
+        color:#fff;
+        height:auto;
+    }
+    .widget_sns_follow > .wrapper{
+        box-sizing:border-box;
+        display:inline-block;
+        height:100%;
+        margin:none;
+        width:49%;
+    }
+    .widget_sns_follow .description{
+        display:block;
+    }
+    .widget_sns_follow .follow-button-area{
+        display:flex;
+        jutify-conent:space-evenly;
+        list-style-type:none;
+    }
+    .widget_sns_follow .follow-button-area > .follow-button{
+        font-size:1.4rem;
+        margin:1em 0;
+        padding:.3em 0 .45em 0;
+        text-align:center;
+        width:32%;
+    }
+    .widget_sns_follow .follow-button-area > .twitter{
+        background-color:#1b95e0;
+    }
+    .widget_sns_follow .follow-button-area > .feedly{
+        background-color:#2BB24C;
+    }
+    .widget_sns_follow .follow-button-area > .twitter:hover{
+        background-color:#31a3ea;
+    }
+    .widget_sns_follow .follow-button-area > .feedly:hover{
+        background-color:#2ebc50;
+    }
+    .widget_sns_follow .follow-button-area > .follow-button > a{
+        color:#fff;
+        text-align:center;
+    }
+</style>
+<img src="' . get_wkwkrnht_eyecatch(array(512,512)) . '" alt="eyrecatch" class="eyecatch wrapper">
+<div class="wrapper">
+    <h3 class="title">' . wp_title('',false) . '</h3>
+    <p class="description">この記事が気に入ったらフォローお願いします！フォロー先で最新情報をお届けします。</p>
+    <ul class="follow-button-area">
+        <li class="twitter follow-button"><a href="https://twitter.com/' . get_twitter_acount() . '" title="Twitterでフォローする"><i class="fa fa-twitter" aria-hidden="true"></i>フォローする</a></li>
+        <li class="feedly follow-button"><a href="' . get_bloginfo('atom_url') . '" title="RSSで購読する"><i class="fa fa-rss" aria-hidden="true"></i>RSSで購読する</a></li>
+    </ul>
+</div>
