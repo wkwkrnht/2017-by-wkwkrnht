7b5d77b05d090d970a77ba007b15cd84f77abfe3
wkwkrnht <wkwkrnht@gmail.com>
2017-07-04 22:28:01 +0900

    fix

--- functions.php
+++ functions.php
@@ -18,7 +18,9 @@ function is_subpage(){
 }
 
 function is_actived_plugin($plugin = ''){
-    require_once(site_url('/includes/plugin.php'));
+    if(!is_admin()){
+        require_once(site_url('/includes/plugin.php'));
+    }
     return is_plugin_active($plugin);
 }
 
