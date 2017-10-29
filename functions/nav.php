56708c326b4212b694f64b352666044bf0ea1bdb
wkwkrnht <wkwkrnht@gmail.com>
2017-10-29 18:46:16 +0900

    fix

--- functions/nav.php
+++ functions/nav.php
@@ -4,8 +4,8 @@ function nav_menu_with_markup($output){
 }
 add_filter('walker_nav_menu_start_el','nav_menu_with_markup',10,4);
 function exted_meta_for_url($atts,$item,$args){
-    $atts['itemprop'] = 'url';
-    $atts['tabindex'] = '0';
+    $atts['itemprop']   = 'url';
+    $atts['tabindex']   = '0';
     $atts['data-title'] = esc_attr($item->title);
 	return $atts;
 }
@@ -17,4 +17,4 @@ function my_nav_menu_item_title($title,$item,$args,$depth){
     }
     return $title;
 }
-add_filter('nav_menu_item_title','social_menu_item_title',10,4);
+add_filter('nav_menu_item_title','social_menu_item_title');
