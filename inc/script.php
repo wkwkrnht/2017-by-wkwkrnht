19e85849c5ea95a3d2a3d6c8ad61436949e3fa59
wkwkrnht <wkwkrnht@gmail.com>
2017-11-25 19:45:01 +0900

    text

--- inc/script.php
+++ inc/script.php
@@ -45,6 +45,8 @@
         document.head.appendChild(wpStyle);
     }
 })();
+<?php //hogehoge?>
+//hogehoge
 <?php if(has_nav_menu('social')===true||has_nav_menu('main')===true||is_active_sidebar('floatmenu')===true):?>
     (function(){
         document.getElementById("menu-toggle").onclick = function(){
